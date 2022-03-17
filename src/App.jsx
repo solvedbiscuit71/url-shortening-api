22f1189f7e8a9ccc83e111097e13cccc8980e0f3
Praveen Perumal <ppraveen98841@gmail.com>
2022-03-17 10:14:45 +0530

    completed @mobile:Hero

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,17 @@ function App(props) {
   return (
     <>
       <Header />
+
+      {/* Hero section */}
+      <section className="hero">
+        <img src="assets/illustration-working.svg" alt="illustration working" className="hero__image" />
+        
+        <h1>More than just shorter links</h1>
+        <p>Build your brands recognition and get detailed insights on how your links are performing.</p>
+
+        <button className="btn--primary">Get Started</button>
+      </section>
+
       <Footer />
     </>
   );
