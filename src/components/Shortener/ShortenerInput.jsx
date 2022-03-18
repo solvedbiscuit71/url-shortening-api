d2900ce46b9e7897df5604ba6badb9c43ab7bdf2
Praveen Perumal <ppraveen98841@gmail.com>
2022-03-18 07:15:31 +0530

    changed the location of the warning message

--- src/components/Shortener/ShortenerInput.jsx
+++ src/components/Shortener/ShortenerInput.jsx
@@ -3,7 +3,11 @@ function ShortenerInput(props) {
     <div className="shortener-input">
       <label htmlFor="shortener-input">
         <input onInput={(event) => props.handleInput(event.target.value)} className={props.showWarning ? "show-warning" : ""} type="text" id="shortener-input" placeholder="Shorten a link here..." />
-        <span style={{display: props.showWarning ? "block" : "none"}}>{props.warningCode === 1 ? "Please add a link" : "Invalid url"}</span>
+        <p style={{display: props.showWarning ? "block" : "none"}}>
+          <span>
+            {props.warningCode === 1 ? "Please add a link" : "Invalid url"}
+          </span>
+        </p>
       </label>
 
       <button className="btn--primary">Shorten It!</button>
