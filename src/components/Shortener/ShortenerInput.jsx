25044a3b38b1c0244aee722fbb21f4251abaf30f
Praveen Perumal <ppraveen98841@gmail.com>
2022-03-18 08:39:05 +0530

    resolve the HTML issue with <label> tag
    Error:
    Element div not allowed as child of element label in this context.
    (Suppressing further errors from this subtree.)
    
    Solution:
    changed the <label> to <div class="input-container">
    and wrap the <input> element only with the <label>

--- src/components/Shortener/ShortenerInput.jsx
+++ src/components/Shortener/ShortenerInput.jsx
@@ -1,14 +1,16 @@
 function ShortenerInput(props) {
   return ( 
     <div className="shortener-input">
-      <label htmlFor="shortener-input">
-        <input onInput={(event) => props.handleInput(event.target.value)} className={props.showWarning ? "show-warning" : ""} type="text" id="shortener-input" placeholder="Shorten a link here..." />
+      <div className="input-container">
+        <label htmlFor="shortener-input">
+          <input onInput={(event) => props.handleInput(event.target.value)} id="shortener-input" className={props.showWarning ? "show-warning" : ""} type="text" placeholder="Shorten a link here..." />
+        </label>
         <div style={{display: props.showWarning ? "block" : "none"}}>
           <span>
             {props.warningCode === 1 ? "Please add a link" : "Invalid url"}
           </span>
         </div>
-      </label>
+      </div>
 
       <button onClick={props.handleClick} className="btn--primary">Shorten It!</button>
     </div>
